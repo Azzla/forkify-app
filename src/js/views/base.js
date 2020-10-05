fd5fb20c47a864e74c3cd8dcb22105b10a47b772
Carson <azzlamusic@hotmail.com>
2020-10-05 13:04:58 -0600

    new string selectors

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -4,7 +4,8 @@ export const elements = {
 	searchResList: document.querySelector('.results__list'),
 	searchRes: document.querySelector('.results'),
 	searchResPages: document.querySelector('.results__pages'),
-	recipe: document.querySelector('.recipe')
+	recipe: document.querySelector('.recipe'),
+	shopping: document.querySelector('.shopping__list')
 };
 
 const elementStrings = {
